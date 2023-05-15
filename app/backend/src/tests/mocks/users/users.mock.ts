e64aa4e58b38c33029305a021c339da5c6761aa2
gmaralan <gmartinialan@gmail.com>
2023-05-15 10:12:16 -0400

    chore: finished user login test

--- app/backend/src/tests/mocks/users/users.mock.ts
+++ app/backend/src/tests/mocks/users/users.mock.ts
@@ -1,20 +1,34 @@
-const VALID_EMAIL = 'definatelyNotSantaClaus@gmail.com';
-const INVALID_EMAIL = 'example.example.com';
+export const VALID_EMAIL =
+  'definatelyNotSantaClaus@gmail.com';
 
-const VALID_PASSWORD = 'lookatthisverycoolpassword';
+export const INVALID_EMAIL =
+  'example.example.com';
+
+export const VALID_PASSWORD =
+  'lookatthisverycoolpassword';
+export const INVALID_PASSWORD = '1234';
 
 const USER = {
   email: VALID_EMAIL,
-  password: VALID_PASSWORD
-}
+  password: VALID_PASSWORD,
+};
 
 const USER_INVALID_EMAIL = {
   email: INVALID_EMAIL,
-  password: VALID_PASSWORD
-}
+  password: VALID_PASSWORD,
+};
 
 const USER_INVALID_PASSWORD = {
   email: VALID_EMAIL,
-  password: VALID_PASSWORD
-}
-export default { USER, USER_INVALID_EMAIL, USER_INVALID_PASSWORD };
+  password: INVALID_PASSWORD,
+};
+
+export default {
+  USER,
+  USER_INVALID_EMAIL,
+  USER_INVALID_PASSWORD,
+  VALID_EMAIL,
+  INVALID_EMAIL,
+  VALID_PASSWORD,
+  INVALID_PASSWORD,
+};
--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -14,7 +14,7 @@ import IUser from '../interfaces/users/IUser.interface';
 import authErrors from './mocks/errors/authErrorsMock.mock';
 import errorsMock from './mocks/errors/errorsMock.mock';
 import tokenMock from './mocks/users/token.mock';
-import usersMock from './mocks/users/users.mock'
+import usersMock from './mocks/users/users.mock';
 
 // Models
 import Users from '../database/models/users.model';
@@ -22,9 +22,9 @@ import Users from '../database/models/users.model';
 chai.use(chaiHttp);
 const { expect } = chai;
 
-afterEach(()=>{
+afterEach(() => {
   sinon.restore();
-})
+});
 
 describe('Teams findAll', () => {
   let chaiHttpResponse: Response;
@@ -32,34 +32,85 @@ describe('Teams findAll', () => {
   describe('Successful returns', function () {
     beforeEach(async () => {
       sinon
-        .stub(Users, "findOne")
+        .stub(Users, 'findOne')
         .resolves(usersMock.USER as IUser);
     });
 
     it('Should return a token', async () => {
       chaiHttpResponse = await chai
-      .request(app)
-      .get('/login');
-      
-      expect(chaiHttpResponse.body).to.deep.equal(tokenMock.token);
-      expect(chaiHttpResponse.status).to.equal(StatusCodes.OK);
-    });
-  })
-  
-  describe('Unsucceful returns', function () {
-    beforeEach(() => {
-      sinon
-        .stub(Teams, 'findAll')
-        .resolves([])
-    });
+        .request(app)
+        .get('/login')
+        .send(usersMock.USER);
 
-    it('Should return internal error', async () => {
-      chaiHttpResponse = await chai
-      .request(app)
-      .get('/teams');
-      
-      expect(chaiHttpResponse.body).to.deep.equal(errorsMock.badImplementationError);
-      expect(chaiHttpResponse.status).to.equal(errorsMock.badImplementationError.statusCode);
+      expect(chaiHttpResponse.body).to.deep.equal(
+        tokenMock.token,
+      );
+      expect(chaiHttpResponse.status).to.equal(
+        StatusCodes.OK,
+      );
     });
-  })
+  });
+
+  describe('Unsucceful returns', function () {
+    it('Should return lacking email error', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/login')
+        .send({
+          password: usersMock.VALID_PASSWORD,
+        });
+
+      expect(chaiHttpResponse.body).to.deep.equal(
+        authErrors.authNoEmailOrPass,
+      );
+      expect(chaiHttpResponse.status).to.equal(
+        StatusCodes.BAD_REQUEST,
+      );
+    });
+    it('Should return lacking password error', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/login')
+        .send({ email: usersMock.VALID_EMAIL });
+
+      expect(chaiHttpResponse.body).to.deep.equal(
+        authErrors.authNoEmailOrPass,
+      );
+      expect(chaiHttpResponse.status).to.equal(
+        StatusCodes.BAD_REQUEST,
+      );
+    });
+    it('Should return invalid email error', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/login')
+        .send({
+          email: usersMock.INVALID_EMAIL,
+          password: usersMock.VALID_PASSWORD,
+        });
+
+      expect(chaiHttpResponse.body).to.deep.equal(
+        authErrors.authInvalidEmailOrPass,
+      );
+      expect(chaiHttpResponse.status).to.equal(
+        StatusCodes.UNAUTHORIZED,
+      );
+    });
+    it('Should return invalid password error', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/login')
+        .send({
+          email: usersMock.VALID_EMAIL,
+          password: usersMock.INVALID_PASSWORD,
+        });
+
+      expect(chaiHttpResponse.body).to.deep.equal(
+        authErrors.authInvalidEmailOrPass,
+      );
+      expect(chaiHttpResponse.status).to.equal(
+        StatusCodes.UNAUTHORIZED,
+      );
+    });
+  });
 });
