96908fcabfd9cbc9b767b86af0d32ee0e5ac4477
gmaralan <gmartinialan@gmail.com>
2023-05-16 07:49:32 -0400

    feat: finished tokenClient class

--- app/backend/src/auth/TokenClient.auth.ts
+++ app/backend/src/auth/TokenClient.auth.ts
@@ -1,4 +1,21 @@
+import ITokenPayload from '../interfaces/auth/ITokenPayload.interface';
+import ITokenProvider from '../interfaces/auth/ITokenProvider.interface';
+import JWTokenProvider from './JWToken.auth';
 
 class TokenClient {
+  private _tokenProvider: ITokenProvider;
 
+  constructor() {
+    this._tokenProvider = new JWTokenProvider();
+  }
+
+  generateToken(payload: ITokenPayload) {
+    return this._tokenProvider.generateToken(payload);
+  }
+
+  verifyToken(token: string) {
+    return this._tokenProvider.verifyToken(token);
+  }
 }
+
+export default TokenClient;
