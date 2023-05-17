6a9ed8c817b3c26fff022cb8d521ac84d8e069d7
gmaralan <gmartinialan@gmail.com>
2023-05-17 11:41:44 -0400

    chore: started working on validation. ValidationClient layer

---
+++ app/backend/src/validation/ValidationClient.class.ts
@@ -0,0 +1,24 @@
+import EnumValidation from '../enums/validation.enum';
+import IValidationProvider from '../interfaces/validation/IValidationProvider.interface';
+import TValidateResult from '../types/TValidateResult.type';
+import JOIProvider from './JOI/JOIProvider.provider';
+
+/*
+  Decide which validation provider to use.
+  This makes easy to change validation method in the future. You just have to
+  create a new provider and insert here.
+*/
+
+class validationClient<T> {
+  validationProvider: IValidationProvider;
+
+  constructor (ruleSet: EnumValidation) {
+    this.validationProvider = new JOIProvider(ruleSet)
+  }
+
+  validate(dataToValidate: T): TValidateResult {
+    return this.validationProvider.validate(dataToValidate);
+  };
+}
+
+export default validationClient;
