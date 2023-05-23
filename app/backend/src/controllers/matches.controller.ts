2082cb803dcd75d65d142884b922222f04267b59
gmaralan <gmartinialan@gmail.com>
2023-05-23 07:39:47 -0400

    chore: controller for PATCH /matches/id

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -5,6 +5,7 @@ import {
 } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import MatchesService from '../services/matches.service';
+import IUpdateGoalsBody from '../interfaces/matches/IUpdateGoalsBody.interface';
 
 export default class MatchesController {
   private matchService;
@@ -14,6 +15,7 @@ export default class MatchesController {
     this.findById = this.findById.bind(this);
     this.findAllByQuery = this.findAllByQuery.bind(this);
     this.finishMatch = this.finishMatch.bind(this);
+    this.updateGoals = this.updateGoals.bind(this);
 
     this.matchService = new MatchesService();
   }
@@ -79,6 +81,23 @@ export default class MatchesController {
       await this.matchService.finishMatch(id);
 
       res.status(StatusCodes.OK).json({ message: 'Finished' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateGoals(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const newGoals: IUpdateGoalsBody = req.body;
+    const { id } = req.params;
+
+    try {
+      const updatedMatch = await this.matchService.updateGoals(id, newGoals);
+
+      res.status(StatusCodes.OK).json({ updatedMatch });
     } catch (error) {
       next(error);
     }
